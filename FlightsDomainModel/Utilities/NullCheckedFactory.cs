7c1633d3b7647c7c7762b793eb9ff19ccf77d60e
Adam <adas@aulendil.net>
2025-03-09 18:13:16 +0100

    null safety utility and pseudo log

---
+++ FlightsDomainModel/Utilities/NullCheckedFactory.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+namespace FlightsDomainModel;
+
+internal static class NullCheckedFactory
+{
+    internal static bool Create<T, MT>(MT dto, out T? created) {
+        created = default;
+        if(dto is null) {
+            Log.Error("Can't create object from null dto");
+            return false;
+        }
+        if(ContainsNulls(dto)) {
+            Log.Error("Wrong dto, contained nulls");
+            return false;
+        }
+        try {
+            created = Construct<T, MT>(dto);
+            return true;
+        } catch {
+            Console.WriteLine($"Wrong use of {nameof(NullCheckedFactory)}, {typeof(T)} doesn't have proper construstor");
+            return false;
+        }
+        
+    }
+
+    static T Construct<T, MT>(MT dto) {
+        var ctor = typeof(T).GetConstructor(new Type[] { typeof(MT) });
+        return (T)ctor.Invoke([dto]);
+    }
+
+    static bool ContainsNulls<T>(T dto) {
+        return typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                   .All(prop => prop.GetValue(dto) != null);
+    }
+}
---
+++ FlightsDomainModel/Utilities/PseudoLog.cs
@@ -0,0 +1,8 @@
+﻿namespace FlightsDomainModel;
+internal static class Log
+{
+    internal static void Error(string msg, Exception? err = null) {
+        //TODO: plug in some serious log
+        Console.WriteLine(msg);
+    }
+}
